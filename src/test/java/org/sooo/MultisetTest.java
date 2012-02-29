a62f9d38a6a4c11ae1848d7e4fb9d30c66013027
Soo Philip Kim <philipjkim@gmail.com>
2012-02-29 08:12:38 -0800

    add test for Multiset and a text file for input resource

---
+++ src/test/java/org/sooo/MultisetTest.java
@@ -0,0 +1,55 @@
+package org.sooo;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Charsets;
+import com.google.common.base.Splitter;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multiset;
+import com.google.common.io.Files;
+
+public class MultisetTest {
+
+	private Iterable<String> words;
+
+	@Before
+	public void before() throws Exception {
+		String text = Files.toString(new File("src/test/resources/sample.txt"),
+				Charsets.UTF_8);
+		words = Splitter.on(CharMatcher.anyOf(" .,?\n")).trimResults()
+				.omitEmptyStrings().split(text);
+	}
+
+	@Test
+	public void countWordViaTraditionalWay() {
+		Map<String, Integer> counts = new HashMap<String, Integer>();
+		for (String word : words) {
+			Integer count = counts.get(word);
+			if (count == null)
+				counts.put(word, 1);
+			else
+				counts.put(word, count + 1);
+		}
+
+		for (String key : counts.keySet())
+			System.out.println(key + "=" + counts.get(key));
+	}
+
+	@Test
+	public void countWordUsingMultiset() {
+		Multiset<String> wordsMultiset = HashMultiset.create();
+		List<String> wordsList = Lists.newArrayList(words);
+		wordsMultiset.addAll(wordsList);
+
+		for (String word : wordsMultiset.elementSet())
+			System.out.println(word + "=" + wordsMultiset.count(word));
+	}
+}
