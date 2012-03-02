b19f01ca7e64f7e95537350badc691af7b5721b3
Soo Philip Kim <philipjkim@gmail.com>
2012-03-02 16:15:57 +0900

    add OptionalTest

---
+++ src/test/java/org/sooo/OptionalTest.java
@@ -0,0 +1,45 @@
+package org.sooo;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import com.google.common.base.Optional;
+
+public class OptionalTest {
+
+	@Test
+	public void useNull() {
+		Customer customer = null;
+		assertNull(customer);
+	}
+
+	@Test
+	public void useFromNullableAndIsPresent() {
+		Optional<Customer> customer = Optional.fromNullable(null);
+		assertFalse(customer.isPresent());
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void useOfToFailFast() {
+		Optional.of(null);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void useAbsentOptional() {
+		Optional<Customer> customer = Optional.fromNullable(null);
+		System.out.println(customer.get().getEmailAddress());
+	}
+
+	@Test
+	public void useOrToUseDefaultObject() {
+		Optional<Customer> customer = Optional.fromNullable(null);
+		System.out.println(customer.or(getDefaultCustomer()).getEmailAddress());
+	}
+
+	private Customer getDefaultCustomer() {
+		return new Customer("John", "Doe", "john.doe@email.com",
+				"010-1234-5678", 1950, false);
+	}
+}
