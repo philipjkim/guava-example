626af2cbe6269fa39fcde9acca19f31b577b0e19
Soo Philip Kim <philipjkim@gmail.com>
2012-04-13 21:16:27 +0900

    add test for MapJoiner

--- src/test/java/org/sooo/base/JoinerTest.java
+++ src/test/java/org/sooo/base/JoinerTest.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import org.junit.Test;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.Maps;
 
 public class JoinerTest {
 
@@ -37,5 +39,15 @@ public class JoinerTest {
 	public void joinUsingJoinerObjectNotInstance() {
 		String joined = Joiner.on(",").join(Arrays.asList(1, 5, 7));
 		assertThat(joined, is("1,5,7"));
+	}
+
+	@Test
+	public void createQueryStringUsingMapJoiner() {
+		Map<String, String> input = Maps.newTreeMap();
+		input.put("id", "id001");
+		input.put("page", "2");
+		input.put("svc", "mail");
+		String result = Joiner.on("&").withKeyValueSeparator("=").join(input);
+		assertThat(result, is("id=id001&page=2&svc=mail"));
 	}
 }
